85fc1d58a1698e5e6d48b524561e800d5fbdd9c9
Li Dai <li.lea.dai@gmail.com>
2020-11-26 15:38:28 -0800

    set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "works#root"
+  get "/auth/github", as: "github_login"
+  get "/auth/:provider/callback", to: "users#create",  as: "omniauth_callback"
+
   get "/login", to: "users#login_form", as: "login"
   post "/login", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"
@@ -9,4 +12,6 @@ Rails.application.routes.draw do
   post "/works/:id/upvote", to: "works#upvote", as: "upvote"
 
   resources :users, only: [:index, :show]
+
+
 end
